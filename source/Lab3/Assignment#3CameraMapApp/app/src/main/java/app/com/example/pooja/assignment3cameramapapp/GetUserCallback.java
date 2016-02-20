f50f974281aa6ae8a37f76c6c6e3aea7865cb0fd
PoojaShekhar <psrp7@mail.umkc.edu>
2016-02-20 02:53:49 -0600

    ..

---
+++ source/Lab3/Assignment#3CameraMapApp/app/src/main/java/app/com/example/pooja/assignment3cameramapapp/GetUserCallback.java
@@ -0,0 +1,8 @@
+package app.com.example.pooja.assignment3cameramapapp;
+
+/**
+ * Created by pooja on 2/10/16.
+ */
+public interface GetUserCallback {
+    public abstract void done(User returnedUser);
+}
